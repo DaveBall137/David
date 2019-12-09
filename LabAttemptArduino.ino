e8ab8c45b9fb3b42ef0a03ee1ebb9083edeaa92d
DaveBall137 <58558646+DaveBall137@users.noreply.github.com>
2019-12-09 16:58:12 +0000

    Update LabAttemptArduino.ino
    Changed the comparison between the motors did a larger average, and reduced the motor speed to work within a lower range, this meant the micromouse has started to run straight with some correction and with a reasonable accuracy, the routine for avoiding needs to be changed so it doesnt get confused but it works successfully.

--- LabAttemptArduino.ino
+++ LabAttemptArduino.ino
@@ -2,51 +2,38 @@
   int FL = 51; //setting pin 51 to the Front Left sensor
   int BL = 52; //setting pin 52 to the Back Left sensor
   int BR = 53; //setting pin 53 to the Back Right sensor
-  int ENA = 2; //Setting pin 2 as a PWM for A
+  
+  int ENA = 10; //Setting pin 2 as a PWM for A changed to 10
   int IN1 = 49; //Setting pin 49 for the MC pin 1
   int IN2 = 48; //Setting pin 48 for the MC pin 2
   int IN3 = 47; //Setting pin 47 for the MC pin 3
   int IN4 = 46; //Setting pin 46 for the MC pin 4
-  int ENB = 3; //Setting pin 3 as a PWM for B
-  int MOTORA = 235;
-  int MOTORB = 235;
-  int tachA = 10;
-  int tachB = 11;
-  int widthA;
-  int widthB;
-  int MA1;
-  int MB1;
-  int MA2;
-  int MB2;  
-  int MA3;
-  int MB3;
-  int MA4;
-  int MB4;
-  int MA5;
-  int MB5;  
-  int MA6;
-  int MB6;
-  int AVwidthA;
-  int AVwidthB;
-
+  int ENB = 11; //Setting pin 3 as a PWM for B changed to 11
+  int MOTORA = 200;
+  int MOTORB = 200;
+  int tachA = 3; // changed from 10 -> 3
+  int tachB = 2; // changed from 11 -> 2
+  int widthA;  int widthB;
+  int MA1;  int MA2;  int MA3;  int MA4;  int MA5;  int MA6;  int MA7;  int MA8;  int MA9;  int MA10;  int MA11;  int MA12; 
+  int MB1;  int MB2;  int MB3;  int MB4;  int MB5;  int MB6;  int MB7;  int MB8;  int MB9;  int MB10;  int MB11;  int MB12; 
+  int AVwidthA;  int AVwidthB;
   
 void setup() {
   // put your setup code here, to run once:
-  pinMode(BR, INPUT);
-  pinMode(BL, INPUT);
-  pinMode(FR, INPUT);
-  pinMode(FL, INPUT);
-  Serial.begin(9600);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  pinMode(ENA, OUTPUT);
-  pinMode(ENB, OUTPUT);
-  pinMode(tachA,INPUT);
-  pinMode(tachB,INPUT);
+    pinMode(BR, INPUT);
+    pinMode(BL, INPUT);
+    pinMode(FR, INPUT);
+    pinMode(FL, INPUT);
+    Serial.begin(9600);
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    pinMode(ENA, OUTPUT);
+    pinMode(ENB, OUTPUT);
+    pinMode(tachA,INPUT);
+    pinMode(tachB,INPUT);
 }
-
 
 void forward(){
   analogWrite(ENA, MOTORA);
@@ -56,7 +43,6 @@ void forward(){
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
   Serial.println("Forward");
-  comparison();
 }
 
 void back() {
@@ -95,40 +81,36 @@ void stop() {
   Serial.println("Stop!");
 }
 
+
+
 void comparison(){
   // Cheching MOTORB is quicker than MOTORA
-  widthA = pulseIn(tachA, HIGH);
-  widthB = pulseIn(tachB, HIGH);
-  MA1=widthA;
-  MB1=widthB;
-  delay(1);
-  widthA = pulseIn(tachA, HIGH);
-  widthB = pulseIn(tachB, HIGH);
-  MA2=widthA;
-  MB2=widthB;
-  delay(1);
-  widthA = pulseIn(tachA, HIGH);
-  widthB = pulseIn(tachB, HIGH);
-  MA3=widthA;
-  MB3=widthB;
-  widthA = pulseIn(tachA, HIGH);
-  widthB = pulseIn(tachB, HIGH);
-  MA4=widthA;
-  MB4=widthB;
-  delay(1);
-  widthA = pulseIn(tachA, HIGH);
-  widthB = pulseIn(tachB, HIGH);
-  MA5=widthA;
-  MB5=widthB;
-  delay(1);
-  widthA = pulseIn(tachA, HIGH);
-  widthB = pulseIn(tachB, HIGH);
-  MA6=widthA;
-  MB6=widthB;
-  AVwidthA = ((MA1 + MA2 + MA3 + MA4 + MA5 + MA6)/6); // 600 is inaccurate, 60 is accurate and 6 is more accurate
-  AVwidthB = ((MB1 + MB2 + MB3 + MB4 + MB5 + MB6)/6);
-  widthA = AVwidthA;
-  widthB = AVwidthB;
+  MA1 = pulseIn(tachA, HIGH);
+  MB1 = pulseIn(tachB, HIGH);
+  MA2 = pulseIn(tachA, HIGH);
+  MB2 = pulseIn(tachB, HIGH);
+  MA3 = pulseIn(tachA, HIGH);
+  MB3 = pulseIn(tachB, HIGH);
+  MA4 = pulseIn(tachA, HIGH);
+  MB4 = pulseIn(tachB, HIGH);
+  MA5 = pulseIn(tachA, HIGH);
+  MB5 = pulseIn(tachB, HIGH);
+  MA6 = pulseIn(tachA, HIGH);
+  MB6 = pulseIn(tachB, HIGH);
+  MA7 = pulseIn(tachA, HIGH);
+  MB7 = pulseIn(tachB, HIGH);
+  MA8 = pulseIn(tachA, HIGH);
+  MB8 = pulseIn(tachB, HIGH);
+  MA9 = pulseIn(tachA, HIGH);
+  MB9 = pulseIn(tachB, HIGH);
+  MA10 = pulseIn(tachA, HIGH);
+  MB10 = pulseIn(tachB, HIGH);
+  MA11 = pulseIn(tachA, HIGH);
+  MB11 = pulseIn(tachB, HIGH);
+  MA12 = pulseIn(tachA, HIGH);
+  MB12 = pulseIn(tachB, HIGH);
+  widthA = ((MA1 + MA2 + MA3 + MA4 + MA5 + MA6 + MA7 + MA8 + MA9 + MA10 + MA11 + MA12)/120); // Averages the values if the value of 60 is increased by a power of 10 it
+  widthB = ((MB1 + MB2 + MB3 + MB4 + MB5 + MB6 + MB7 + MB8 + MB9 + MB10 + MB11 + MB12)/120); //reduces the accuracy
   Serial.print("This is widthA: ");
   Serial.println(widthA);
   Serial.print("This is MOTORA: ");
@@ -137,28 +119,23 @@ void comparison(){
   Serial.println(widthB);
   Serial.print("This is MOTORB: ");
   Serial.println(MOTORB);
-
-    if((widthA > widthB) && (MOTORA < 250)){
-      MOTORA++;
-      }
-    else if((widthA > widthB) && (MOTORA >= 250 )){
-      MOTORB--;
-      MOTORB--;
-      MOTORA--;
-      }      
-  // Cheching MOTORA is quicker than MOTORB
-    else if((widthA < widthB) && (MOTORB < 250)){
-      MOTORB == MOTORB++;
-      }
-    else if((widthA < widthB) && (MOTORB >= 250 )){
-      MOTORA--;
-      MOTORA--;;
-      MOTORB--;
-      }
-    else{
-      //do nothing
-      Serial.println("SYNCHED");
-      }
+  
+  if((widthA > widthB) && (MOTORB >= 150)){
+    MOTORB--;
+    }
+  else if((widthA < widthB) && (MOTORB < 250)){
+    MOTORB++;
+    }
+  else if((widthA < widthB) && (MOTORB >= 250 )){
+    MOTORB -= 10;
+    }
+  else if(MOTORB < 150 ){
+    MOTORA == 0;
+    MOTORB == 0;
+    }  
+  else{
+    Serial.println("SYNCHED");
+    }
 }
 
 
@@ -193,15 +170,18 @@ void obstacleavoidance(){
     stop();
     delay(10);
     forward();
+    comparison();
     delay(10);
     }
   else{
     forward();
+    comparison();
     delay(10);
 }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  obstacleavoidance(); 
+    obstacleavoidance();
+
 }
