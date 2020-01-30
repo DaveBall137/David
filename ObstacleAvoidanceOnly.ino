c14651ad7fc37fa932d6741014a0fcb417bf81f8
DaveBall137 <58558646+DaveBall137@users.noreply.github.com>
2020-01-30 17:33:42 +0000

    Update ObstacleAvoidanceOnly.ino

--- ObstacleAvoidanceOnly.ino
+++ ObstacleAvoidanceOnly.ino
@@ -12,8 +12,8 @@
   int MOTORA;
   int MOTORB;
   
-  int tachA = 3; // changed from 10 -> 3
-  int tachB = 2; // changed from 11 -> 2
+  int tachA = 2; // changed from 10 -> 3
+  int tachB = 3; // changed from 11 -> 2
   
   int widthA;  int widthB;
   int MA1;  int MA2;  int MA3;  int MA4;  int MA5;  int MA6;  int MA7;  int MA8;  int MA9;  int MA10;  int MA11;  int MA12; 
