569285c6c966901efde9d27355a98735339b76d5
DaveBall137 <58558646+DaveBall137@users.noreply.github.com>
2020-01-30 15:38:06 +0000

    Update Tachometer control.ino
    Update before testing

--- Tachometer control.ino
+++ Tachometer control.ino
@@ -1,55 +1,82 @@
-  int ENA = 2; //Setting pin 2 as a PWM for A
+  int FR = 50; //setting pin 50 to the Front Right sensor
+  int FL = 51; //setting pin 51 to the Front Left sensor
+  int BL = 52; //setting pin 52 to the Back Left sensor
+  int BR = 53; //setting pin 53 to the Back Right sensor
+  
+  int ENA = 10; //Setting pin 2 as a PWM for A changed to 10
   int IN1 = 49; //Setting pin 49 for the MC pin 1
   int IN2 = 48; //Setting pin 48 for the MC pin 2
   int IN3 = 47; //Setting pin 47 for the MC pin 3
   int IN4 = 46; //Setting pin 46 for the MC pin 4
-  int ENB = 3; //Setting pin 3 as a PWM for B
-  int MOTORA = 200;
-  int MOTORB = 200;
-  int tachA = 10;
-  int tachB = 11;
- /* unsigned long widthA;
-  unsigned long widthB; */
-  int widthA;
-  int widthB;
-  int MA1;
-  int MA2;
-  int MA3;
-  int MA4;
-  int MA5;
-  int MA6;
-  int MA7;
-  int MA8;
-  int MA9;
-  int MA10;
-  int MA11;
-  int MA12; 
-  int MB1;
-  int MB2;
-  int MB3;
-  int MB4;
-  int MB5;
-  int MB6;
-  int MB7;
-  int MB8;
-  int MB9;
-  int MB10;
-  int MB11;
-  int MB12; 
-  int AVwidthA;
-  int AVwidthB;
+  int ENB = 11; //Setting pin 3 as a PWM for B changed to 11
+  int MOTORA;
+  int MOTORB;
+  
+  int tachA = 3; // changed from 10 -> 3
+  int tachB = 2; // changed from 11 -> 2
+  
+  int widthA;  int widthB;
+  int MA1;  int MA2;  int MA3;  int MA4;  int MA5;  int MA6;  int MA7;  int MA8;  int MA9;  int MA10;  int MA11;  int MA12; 
+  int MB1;  int MB2;  int MB3;  int MB4;  int MB5;  int MB6;  int MB7;  int MB8;  int MB9;  int MB10;  int MB11;  int MB12; 
+  int AVwidthA;  int AVwidthB;
+  
+  
+  int RWL = A0; //setting pin White line middle to pin 24
+  int MWL = A1; //setting pin White line middle to pin 24
+  int LWL = A2; //setting pin White line middle to pin 24
+  int PWL = 23;
+  int ZWL = 22;
+  int LeftWhite;
+  int MiddleWhite;
+  int RightWhite;
+  int LeftBlack;
+  int MiddleBlack;
+  int RightBlack;
+  int ThresholdComplete; 
+  int LeftTest = 0;
+  int MiddleTest = 0;
+  int RightTest = 0;
+  float ThreshL;
+  float ThreshM;
+  float ThreshR;
+   
+
   
 void setup() {
   // put your setup code here, to run once:
-   Serial.begin(9600);
-    pinMode(IN1, OUTPUT);
-    pinMode(IN2, OUTPUT);
-    pinMode(IN3, OUTPUT);
-    pinMode(IN4, OUTPUT);
-    pinMode(ENA, OUTPUT);
-    pinMode(ENB, OUTPUT);
-    pinMode(tachA,INPUT);
-    pinMode(tachB,INPUT);   
+  pinMode(PWL, OUTPUT);
+  pinMode(ZWL, OUTPUT);
+  pinMode(BR, INPUT);
+  pinMode(BL, INPUT);
+  pinMode(FR, INPUT);
+  pinMode(FL, INPUT);
+  Serial.begin(9600);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(tachA,INPUT);
+  pinMode(tachB,INPUT);
+  digitalWrite(PWL, HIGH);
+  digitalWrite(ZWL, LOW);
+  Serial.begin(9600);
+  LeftWhite = 0;
+  MiddleWhite = 0;
+  RightWhite = 0;
+  LeftBlack = 0;
+  MiddleBlack = 0;
+  RightBlack = 0;
+  LeftTest = 0;
+  MiddleTest = 0;
+  RightTest = 0;
+  ThresholdComplete = 0;
+  int ThreshL = 0;
+  int ThreshM = 0;
+  int ThreshR = 0;
+  MOTORA = 200;
+  MOTORB = 200; 
 }
 
  void forward(){
