591c59c33241050839a28f5122b59884c6a130ce
DaveBall137 <58558646+DaveBall137@users.noreply.github.com>
2020-02-23 17:06:41 +0000

    Update BatteryMonitoring.ino
    Removing unnecessary lines

--- BatteryMonitoring.ino
+++ BatteryMonitoring.ino
@@ -1,11 +1,7 @@
-float ReadVoltage = 0;
 float voltage = 0;
-float threshold = 5.5; //Threshold depending on the battery to determine low battery SoC
+float threshold = 5.9; //Threshold depending on the battery to determine low battery SoC
 float v_read = A3;
 float Adjustment = 0.110;
-
-
-
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,10 +12,9 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:  
-  ReadVoltage = analogRead(v_read);
   //Serial.print("Read Voltage:"); //Prints to monitor
   //Serial.println(ReadVoltage); // Only needed for testing
-  voltage = ((ReadVoltage*(7.5/1023))- Adjustment);
+  voltage = ((analogRead(v_read)*(7.5/1023))- Adjustment);
   Serial.print("Voltage:"); //Prints to monitor
   Serial.println(voltage);
 
